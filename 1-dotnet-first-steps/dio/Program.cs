3b790452aa74c97fc91cb98dd9181ce0e3e106a7
eduardo <eduardormonteiro@gmail.com>
2021-03-01 17:51:55 -0300

    .

---
+++ 1-dotnet-first-steps/dio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using EstruturaDoPrograma.Exemplos;
+namespace dio
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            var s = new Pilha();
+            s.Empilha(1);
+            s.Empilha(10);
+            s.Empilha(100);
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+        }
+
+        // static void Main(string[] args)
+        // {
+        //     int times = 5;
+
+        //     for(int i= 0; i < times; i++)
+        //     {
+        //         Console.WriteLine($"number of times {i}");
+        //     }
+        //     Console.WriteLine("Hello World!");
+        // }
+
+    }
+}
