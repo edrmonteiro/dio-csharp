b31449217cb4717ec898aa66d94ff70e7ecd883d
edrmonteiro <eduardormonteiro@gmail.com>
2021-04-16 23:47:36 -0300

    testes

--- 2-dotnet-app-dev/original/CursoAPI/Controllers/CategoriasController.cs
+++ 2-dotnet-app-dev/original/CursoAPI/Controllers/CategoriasController.cs
@@ -1,110 +1,110 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
-//using CursoMVC.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CursoMVC.Models;
 
-//namespace CursoAPI.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class CategoriasController : ControllerBase
-//    {
-//        private readonly Context _context;
-        
-//        public CategoriasController(Context context)
-//        {
-//            _context = context;
-//        }
+namespace CursoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly Context _context;
 
-//        // GET: api/Categorias
-//        [HttpGet]
-//        public async Task<ActionResult<IEnumerable<Categoria>>> GetCategorias()
-//        {
-//            return await _context.Categorias.ToListAsync();
-//        }
+        public CategoriasController(Context context)
+        {
+            _context = context;
+        }
 
-//        // GET: api/Categorias/5
-//        [HttpGet("{id}")]
-//        public async Task<ActionResult<Categoria>> GetCategoria(int id)
-//        {
-//            var categoria = await _context.Categorias.FindAsync(id);
-            
-//            if (categoria == null)
-//            {
-//                return NotFound();
-//            }
+        // GET: api/Categorias
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categoria>>> GetCategorias()
+        {
+            return await _context.Categorias.ToListAsync();
+        }
 
-//            return categoria;
-//        }
+        // GET: api/Categorias/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Categoria>> GetCategoria(int id)
+        {
+            var categoria = await _context.Categorias.FindAsync(id);
 
-//        // PUT: api/Categorias/5
-//        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-//        // more details see https://aka.ms/RazorPagesCRUD.
-//        [HttpPut("{id}")]
-//        public async Task<IActionResult> PutCategoria(int id, Categoria categoria)
-//        {
-//            if (id != categoria.Id)
-//            {
-//                return BadRequest();
-//            }
+            if (categoria == null)
+            {
+                return NotFound();
+            }
 
-//            _context.SetModified(categoria);
+            return categoria;
+        }
 
-//            try
-//            {
-//                await _context.SaveChangesAsync();
-//            }
-//            catch (DbUpdateConcurrencyException)
-//            {
-//                if (!CategoriaExists(id))
-//                {
-//                    return NotFound();
-//                }
-//                else
-//                {
-//                    throw;
-//                }
-//            }
+        // PUT: api/Categorias/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategoria(int id, Categoria categoria)
+        {
+            if (id != categoria.Id)
+            {
+                return BadRequest();
+            }
 
-//            return NoContent();
-//        }
+            _context.SetModified(categoria);
 
-//        // POST: api/Categorias
-//        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-//        // more details see https://aka.ms/RazorPagesCRUD.
-//        [HttpPost]
-//        public async Task<ActionResult<Categoria>> PostCategoria(Categoria categoria)
-//        {
-//            _context.Categorias.Add(categoria);
-            
-//            await _context.SaveChangesAsync();
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoriaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
 
-//            return CreatedAtAction("GetCategoria", new { id = categoria.Id }, categoria);
-//        }
+            return NoContent();
+        }
 
-//        // DELETE: api/Categorias/5
-//        [HttpDelete("{id}")]
-//        public async Task<ActionResult<Categoria>> DeleteCategoria(int id)
-//        {
-//            var categoria = await _context.Categorias.FindAsync(id);
-//            if (categoria == null)
-//            {
-//                return NotFound();
-//            }
+        // POST: api/Categorias
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Categoria>> PostCategoria(Categoria categoria)
+        {
+            _context.Categorias.Add(categoria);
 
-//            _context.Categorias.Remove(categoria);
-//            await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
 
-//            return categoria;
-//        }
+            return CreatedAtAction("GetCategoria", new { id = categoria.Id }, categoria);
+        }
 
-//        private bool CategoriaExists(int id)
-//        {
-//            return _context.Categorias.Any(e => e.Id == id);
-//        }
-//    }
-//}
+        // DELETE: api/Categorias/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Categoria>> DeleteCategoria(int id)
+        {
+            var categoria = await _context.Categorias.FindAsync(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categorias.Remove(categoria);
+            await _context.SaveChangesAsync();
+
+            return categoria;
+        }
+
+        private bool CategoriaExists(int id)
+        {
+            return _context.Categorias.Any(e => e.Id == id);
+        }
+    }
+}
